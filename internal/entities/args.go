530ca55896b74263f4e436c47a575865289d8387
kourtnet <itromart@gmail.com>
2025-09-22 16:16:14 +0300

    mvp: moved arg functions for separate file for better readability

--- internal/entities/args.go
+++ internal/entities/args.go
@@ -1,15 +1,6 @@
 // Package entities provides entities used in the project: system info arguments and OS logos
 package entities
 
-import (
-	"bufio"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
-	"time"
-)
-
 type Arg struct {
 	Name     string
 	Command  func() (string, error)
@@ -18,139 +9,39 @@ type Arg struct {
 
 var ArgsList = map[string]Arg{
 	"os": {
-		Name: `OS`,
-		Command: func() (string, error) {
-			file, err := os.Open("/etc/os-release")
-			if err != nil {
-				panic(err)
-			}
-
-			defer file.Close()
-
-			scanner := bufio.NewScanner(file)
-			var res string
-
-			for scanner.Scan() {
-				text := scanner.Text()
-				if strings.HasPrefix(text, "PRETTY_NAME=") {
-					suffix := text[len("PRETTY_NAME="):]
-					res = strings.Trim(suffix, `"`) + "\n"
-
-					break
-				}
-			}
-
-			if err := scanner.Err(); err != nil {
-				return "", err
-			}
-
-			return res, nil
-		},
+		Name:    "OS",
+		Command: getOS,
 	},
 
 	"kernel": {
-		Name: `Kernel`,
-		Command: func() (string, error) {
-			kernelByte, err := os.ReadFile("/proc/sys/kernel/osrelease")
-			if err != nil {
-				return "", nil
-			}
-
-			kernel := strings.TrimSpace(string(kernelByte)) + "\n"
-
-			return kernel, nil
-		},
+		Name:    "Kernel",
+		Command: getKernel,
 	},
 
 	"shell": {
-		Name: `Shell`,
-		Command: func() (string, error) {
-			shellPath := os.Getenv("SHELL")
-			shell := filepath.Base(shellPath) + "\n"
-
-			return shell, nil
-		},
+		Name:    "Shell",
+		Command: getShell,
 	},
 
 	"terminal": {
-		Name: `Terminal`,
-		Command: func() (string, error) {
-			term := os.Getenv("TERM") + "\n"
-			return term, nil
-		},
+		Name:    "Terminal",
+		Command: getTerminal,
 	},
 
 	"uptime": {
-		Name: `Uptime`,
-		Command: func() (string, error) {
-			fileByte, err := os.ReadFile("/proc/uptime")
-			if err != nil {
-				return "", nil
-			}
-
-			uptimeStr := strings.Fields(string(fileByte))[0]
-			dur, err := time.ParseDuration(uptimeStr + "s")
-			if err != nil {
-				return "", nil
-			}
-
-			days := int(dur.Hours() / 24)
-			hours := int(dur.Hours()) - days*24
-			minutes := int(dur.Minutes()) - hours*60
-
-			var res string
-
-			if days > 0 {
-				res += strconv.Itoa(days)
-				if days > 1 {
-					res += " days "
-				} else {
-					res += " day "
-				}
-			}
-
-			if hours > 0 {
-				res += strconv.Itoa(hours)
-				if hours > 1 {
-					res += " hours "
-				} else {
-					res += " hour "
-				}
-			}
-
-			if minutes > 0 {
-				res += strconv.Itoa(minutes)
-				if minutes > 1 {
-					res += " minutes "
-				} else {
-					res += " minute "
-				}
-			}
-
-			return res + "\n", nil
-		},
+		Name:    "Uptime",
+		Command: getUptime,
 	},
 
 	"palette_bg": {
-		Name: ``,
-		Command: func() (string, error) {
-			output := "\033[2D\033[40m   \033[41m   \033[42m   \033[43m   \033[44m   \033[45m   \033[46m   \033[47m   \033[0m\n"
-			return output, nil
-		},
+		Command: getPaletteBg,
 	},
 
 	"palette_fg": {
-		Name: ``,
-		Command: func() (string, error) {
-			output := "\033[2D\033[90m███\033[91m███\033[92m███\033[93m███\033[94m███\033[95m███\033[96m███\033[97m███\033[0m\n"
-			return output, nil
-		},
+		Command: getPaletteFg,
 	},
 
 	"indent": {
-		Name: ``,
-		Command: func() (string, error) {
-			return "\033[2D \n", nil
-		},
+		Command: getIndent,
 	},
 }
---
+++ internal/entities/funcs.go
@@ -0,0 +1,123 @@
+package entities
+
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func getOS() (string, error) {
+	file, err := os.Open("/etc/os-release")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var res string
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.HasPrefix(text, "PRETTY_NAME=") {
+			suffix := text[len("PRETTY_NAME="):]
+			res = strings.Trim(suffix, `"`) + "\n"
+
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	return res, nil
+}
+
+func getKernel() (string, error) {
+	kernelByte, err := os.ReadFile("/proc/sys/kernel/osrelease")
+	if err != nil {
+		return "", nil
+	}
+
+	kernel := strings.TrimSpace(string(kernelByte)) + "\n"
+
+	return kernel, nil
+}
+
+func getShell() (string, error) {
+	shellPath := os.Getenv("SHELL")
+	shell := filepath.Base(shellPath) + "\n"
+
+	return shell, nil
+}
+
+func getTerminal() (string, error) {
+	term := os.Getenv("TERM") + "\n"
+	return term, nil
+}
+
+func getUptime() (string, error) {
+	fileByte, err := os.ReadFile("/proc/uptime")
+	if err != nil {
+		return "", nil
+	}
+
+	uptimeStr := strings.Fields(string(fileByte))[0]
+	dur, err := time.ParseDuration(uptimeStr + "s")
+	if err != nil {
+		return "", nil
+	}
+
+	days := int(dur.Hours() / 24)
+	hours := int(dur.Hours()) - days*24
+	minutes := int(dur.Minutes()) - hours*60
+
+	var res string
+
+	if days > 0 {
+		res += strconv.Itoa(days)
+		if days > 1 {
+			res += " days "
+		} else {
+			res += " day "
+		}
+	}
+
+	if hours > 0 {
+		res += strconv.Itoa(hours)
+		if hours > 1 {
+			res += " hours "
+		} else {
+			res += " hour "
+		}
+	}
+
+	if minutes > 0 {
+		res += strconv.Itoa(minutes)
+		if minutes > 1 {
+			res += " minutes"
+		} else {
+			res += " minute"
+		}
+	}
+
+	return res + "\n", nil
+}
+
+func getPaletteBg() (string, error) {
+	output := "\033[2D\033[40m   \033[41m   \033[42m   \033[43m   \033[44m   \033[45m   \033[46m   \033[47m   \033[0m\n"
+	return output, nil
+}
+
+func getPaletteFg() (string, error) {
+	output := "\033[2D\033[90m███\033[91m███\033[92m███\033[93m███\033[94m███\033[95m███\033[96m███\033[97m███\033[0m\n"
+	return output, nil
+}
+
+func getIndent() (string, error) {
+	return "\033[2D \n", nil
+}
