ee4d9ad534b5fe97b7493c7502f82a71174e1e0b
kourtnet <itromart@gmail.com>
2025-09-23 11:43:44 +0300

    mvp: added benchmark test for sysinfo to check the speed of sequential and concurrent fetch. Sequential turned out to be faster

---
+++ internal/sysinfo/sysinfo_test.go
@@ -0,0 +1,32 @@
+package sysinfo
+
+import (
+	"testing"
+
+	"github.com/kourtnet/dummyfetch/internal/entities"
+)
+
+func fetchNArgs(b *testing.B, argNames []string) {
+	for b.Loop() {
+		Fetch(argNames)
+		b.StopTimer()
+
+		for _, v := range argNames {
+			arg := entities.ArgsMap[v]
+			arg.Contents = ""
+			entities.ArgsMap[v] = arg
+		}
+
+		b.StartTimer()
+	}
+}
+
+func BenchmarkFetch1Arg(b *testing.B) {
+	argNames := []string{"kernel"}
+	fetchNArgs(b, argNames)
+}
+
+func BenchmarkFetch5Args(b *testing.B) {
+	argNames := []string{"kernel", "os", "terminal", "uptime", "shell"}
+	fetchNArgs(b, argNames)
+}
