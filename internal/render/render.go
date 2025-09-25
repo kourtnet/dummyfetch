85e548155e30c06556c93700e94ac2014e252031
kourtnet <itromart@gmail.com>
2025-09-25 21:24:15 +0300

    style_vars: implemented style vars to stylize module titles and distro icons

--- internal/render/render.go
+++ internal/render/render.go
@@ -3,12 +3,85 @@ package render
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/kourtnet/dummyfetch/internal/entities"
 	"github.com/kourtnet/dummyfetch/internal/flags"
 )
 
+var styleVars = map[string]string{
+	// Background colors
+	"bg0": "\033[30m", // black
+	"bg1": "\033[31m", // red
+	"bg2": "\033[32m", // green
+	"bg3": "\033[33m", // yellow
+	"bg4": "\033[34m", // blue
+	"bg5": "\033[35m", // magenta
+	"bg6": "\033[36m", // cyan
+	"bg7": "\033[37m", // white
+
+	// Foreground colors
+	"fg0": "\033[90m", // black
+	"fg1": "\033[91m", // red
+	"fg2": "\033[92m", // green
+	"fg3": "\033[93m", // yellow
+	"fg4": "\033[94m", // blue
+	"fg5": "\033[95m", // magenta
+	"fg6": "\033[96m", // cyan
+	"fg7": "\033[97m", // white
+
+	// Text styles
+	"b": "\033[1m", // bold
+	"d": "\033[2m", // dim
+	"i": "\033[3m", // italic
+	"u": "\033[4m", // underline
+	"r": "\033[7m", // reverse
+	"s": "\033[9m", // strikethrough
+
+	// Reset all styles
+	"reset": "\033[0m",
+}
+
+var styleVarRegex = regexp.MustCompile(`\$\{([^}]+)\}`)
+
+func prepareStr(str string) string {
+	res := styleVarRegex.ReplaceAllStringFunc(str, func(match string) string {
+		varName := strings.TrimPrefix(strings.TrimSuffix(match, "}"), "${")
+		if ansi, ok := styleVars[varName]; ok {
+			return ansi
+		}
+
+		return match
+	})
+
+	return res
+}
+
+func prepareLogo() {
+	if flags.Config.LogoName != entities.CustomName {
+		return
+	}
+
+	for i, str := range entities.LogosMap[entities.CustomName].Logo {
+		entities.LogosMap[entities.CustomName].Logo[i] = prepareStr(str)
+	}
+}
+
+func prepareModules() {
+	for i, mod := range flags.Config.Modules {
+		flags.Config.Modules[i].Title = prepareStr(mod.Title)
+	}
+}
+
+func prepare() {
+	prepareModules()
+	prepareLogo()
+}
+
 func Render() {
+	prepare()
+
 	logo := entities.LogosMap[flags.Config.LogoName]
 
 	maxLen := max(len(logo.Logo), len(flags.Config.Modules))
